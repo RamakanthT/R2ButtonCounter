6df57bf212291c62e3ed3ed0e596a74101328876
RamakanthT <ram.0063@gmail.com>
2020-05-22 01:32:36 +0530

    Button counter initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React,{Component} from 'react';
+
+export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      counter : 0,
+      hasButtonBeenClicked: false
+    }
+  }
+
+  render= ()=>
+    <button onClick={this.handleClick} 
+      className={this.props.className} 
+      disabled={this.props.disabled === "true" || this.props.disabled === true}>
+        {this.props.text}{this.state.counter}
+        {this.state.hasButtonBeenClicked && <div>Button Clicked</div> }
+    </button>
+
+  handleClick=()=>{
+    this.setState({counter: this.state.counter + 1},
+      ()=> this.setState({ hasButtonBeenClicked: this.state.counter>0})
+    );
+
+  }
+
+  /* Need to check why this is not working */
+  /* handleClick=()=>{
+    this.setState({counter: this.state.counter + 1,
+      hasButtonBeenClicked: this.state.counter>0 });
+
+  } */
+        
+}
